d6366ba19ff7632687a2f2a448d94c09a71db7f9
Alex <rct.mancevic@bsu.by>
2021-11-05 16:07:37 +0300

    кнопка res

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -2,11 +2,18 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
 import java.text.AttributedCharacterIterator;
 
+
 public class Calculator extends JFrame {
+    private static final int WIDTH = 400;
+    private static final int HEIGHT = 320;
+
+
     private double mem1= 0;
     private double mem2= 0;
     private double mem3= 0;
@@ -16,19 +23,31 @@ public class Calculator extends JFrame {
     private JRadioButton func1 = new JRadioButton("Функция 1");
     private JRadioButton func2 = new JRadioButton("Функция 2");
     private JLabel result = new JLabel("Результат: ");
-    private JLabel input = new JLabel(" 4", 10);
+    private JLabel input = new JLabel(" ", 10);
     private JTextField mem1_1 = new JTextField("", 10);
     private JTextField mem2_2 = new JTextField("", 10);
     private JTextField mem3_3 = new JTextField("", 10);
+    private  JButton M = new JButton("M+");
+    private  JButton MC = new JButton("MC");
+    private  JButton res = new JButton("Вычислить");
+
+
+
 
 
 
     public Calculator() {
 
         super("Calculator");
-        this.setBounds(100, 100, 100,100);
+       // this.setBounds(100, 100, 100,100);
+        setSize(WIDTH, HEIGHT);
+        Toolkit kit = Toolkit.getDefaultToolkit();
+// Центрировать окно приложения на экране
+        setLocation((kit.getScreenSize().width - WIDTH)/2,
+                (kit.getScreenSize().height - HEIGHT)/2);
+
         Container cont = this.getContentPane();
-        cont.setLayout( new GridLayout(5, 1, 2,2));
+        cont.setLayout( new GridLayout(6, 1, 2,2));
       //  BorderLayout myLayout = new BorderLayout(5, 5);
         JPanel panel1 = new JPanel();
         panel1.setLayout(new FlowLayout( FlowLayout.CENTER ));
@@ -64,10 +83,58 @@ public class Calculator extends JFrame {
         mems_text.add(mem3_3);
         mems_text.setLayout(new FlowLayout( FlowLayout.CENTER ));
 
+        JPanel Butt = new JPanel();
+        Butt.add (M);
+        Butt.add (MC);
+        Butt.setLayout(new FlowLayout( FlowLayout.CENTER ));
+
+       // JPanel result_1 = new JPanel();
+       // result_1.add (result);
+       // result_1.add (result_panel);
+       // result_1.setLayout(new FlowLayout( FlowLayout.CENTER ));
+
         cont.add (panel1, BorderLayout.CENTER);
         cont.add (mems, BorderLayout.CENTER);
         cont.add (mems_text, BorderLayout.CENTER);
         cont.add (panel2, BorderLayout.CENTER);
+        cont.add (Butt,BorderLayout.CENTER);
+        cont.add (res,BorderLayout.CENTER );
+
+        res.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try
+                {
+                   Double x = Double.parseDouble(mem1_1.getText());
+                   Double y = Double.parseDouble(mem2_2.getText());
+                   Double z = Double.parseDouble(mem3_3.getText());
+                   Double resultat;
+
+                   if ( func1.isSelected())
+                   {
+                       resultat = f1 (x,y,z);
+
+                   }
+                   else { resultat = f2 (x,y,x);}
+                   input.setText( String.valueOf(resultat));
+
+                }
+                catch (NumberFormatException ex) {
+                    JOptionPane.showMessageDialog(Calculator.this,
+                            "Ошибка в формате записи числа с плавающей точкой", "Ошибочный формат числа",
+                            JOptionPane.WARNING_MESSAGE);
+                }
+
+            }
+        });
+        M.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
@@ -87,4 +154,21 @@ public class Calculator extends JFrame {
     void setMem1 (double mem) { mem1 = mem; }
     void setMem2 (double mem) { mem2 = mem; }
     void setMem3 (double mem) { mem3 = mem; }
-}
+    double f1 (double x, double y, double z)
+    {
+
+
+        return ( (Math.sin(y) + Math.pow (y,2) + Math.exp( Math.cos(y))) * (Math.pow ((Math.log(z) + Math.sin(Math.PI * x*x)), 0.25)) );
+
+    }
+    double f2 (double x, double y, double z)
+    {
+
+        return ( Math.pow (y + Math.pow (x,3),  (1/z) ) / Math.log (z)  );
+
+    }
+
+    }
+
+
+
